fac73c2b00bf7a4189e6105f5a8a8bbba71a1a73
Oskar Gawlak <oskar.gawlak@gmail.com>
2018-07-26 01:23:54 +0200

    Add logo, email and password text inputs, button to login screen

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Splash from './components/Splash';
+import Login from './components/Login';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Splash />
+        <Login />
       </View>
     );
   }
@@ -16,5 +17,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-  }
-})
+  },
+});
--- components/Login.js
+++ components/Login.js
@@ -1,15 +1,103 @@
 import React, { Component } from 'react';
 import {
-    StyleSheet, Text, View, Image,
-    TouchableWithoutFeedback, StatusBar,
-    TextInput, SafeAreaView, Keyboard, TouchableOpacity,
-    KeyboardAvoidingView
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableWithoutFeedback,
+  StatusBar,
+  TextInput,
+  Keyboard,
+  TouchableOpacity,
+  KeyboardAvoidingView,
 } from 'react-native';
 
 export default class Login extends Component {
-    render() {
-        return (
-            
-        );
-    }
+  render() {
+    return (
+      <View style={styles.container}>
+        <KeyboardAvoidingView behavior="padding" style={styles.container}>
+          <TouchableWithoutFeedback
+            style={styles.container}
+            onPress={Keyboard.dismiss}
+          >
+            <View style={styles.logoContainer}>
+              <Image
+                style={styles.logo}
+                source={require('../images/chili.png')}
+              />
+              <Text style={styles.title}>Account Information</Text>
+              <View style={styles.infoContainer}>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Enter username/email"
+                  placeholderTextColor="rgba(255,255,255,0.8)"
+                  keyboardType="email-address"
+                  returnKeyType="next"
+                  autoCorrect={false}
+                  onSubmitEditing={() => this.refs.txtPassword.focus()}
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder="Enter password"
+                  placeholderTextColor="rgba(255,255,255,0.8)"
+                  secureTextEntry
+                  ref={'txtPassword'}
+                />
+                <TouchableOpacity style={styles.buttonContainer}>
+                  <Text style={{ alignSelf: 'center' }}>SIGN IN</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    flex: 1,
+    backgroundColor: 'rgb(32, 53, 70)',
+  },
+  logoContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    color: '#f7c744',
+    fontSize: 18,
+    textAlign: 'center',
+    marginTop: 5,
+    opacity: 0.9,
+  },
+  infoContainer: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 0,
+    height: 200,
+    padding: 20,
+  },
+  input: {
+    height: 40,
+    width: 300,
+    backgroundColor: 'rgba(255, 255, 255,0.2)',
+    color: '#FFF',
+    marginBottom: 20,
+    paddingHorizontal: 10,
+  },
+  buttonContainer: {
+    backgroundColor: '#f7c744',
+    paddingVertical: 15,
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: 'rgb(32,53,70)',
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+});
